630f60ba002f67c13af44384b4cf14881c56257b
Masato Linaker <masato.linaker@gmail.com>
2026-06-14 13:38:08 -0700

    Activity object & controller created then finished. Migration initialized, Db created, and builder services provided in the program file.

---
+++ ExamTakeHomeLvl7/Controllers/ActivityController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExamTakeHomeLvl7.Models;
+
+namespace ExamTakeHomeLvl7.Controllers
+{
+    public class ActivityController : Controller
+    {
+        readonly ApplicationDbContext _db;
+
+        public ActivityController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Activities.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Name, Date")] Activity activity)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Activities.Add(activity);
+                _db.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(activity);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            
+            var activity = _db.Activities.FirstOrDefault(x => x.Id == id);
+
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        [HttpPost]
+
+        public IActionResult Delete(int id)
+        {
+            var activity = _db.Activities.FirstOrDefault(x => x.Id == id);
+
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            _db.Activities.Remove(activity);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ ExamTakeHomeLvl7/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ExamTakeHomeLvl7.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("FileName = ./ActivityTracker.db");
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
